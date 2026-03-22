1f78176cf8d691e53f8af4af768e9f5c6dfbe095
Sahya357 <97974781+Sahya357@users.noreply.github.com>
2026-03-22 20:22:36 +0200

    Update episode selection and release dates

--- Lab3(3.21)/Lab3(3.21)/Lab3(3.21).cpp
+++ Lab3(3.21)/Lab3(3.21)/Lab3(3.21).cpp
@@ -1,4 +1,4 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <locale.h>
 
 int main() {
@@ -9,52 +9,36 @@ int main() {
 	scanf_s("%d", &TrueDetevctiv);
     switch(TrueDetevctiv) {
         case 1:
-            printf("Ви вибрали серію %d\n",TrueDetevctiv);
-            printf("Дата виходу %d.01.2026", TrueDetevctiv);
+            printf("Ви вибрали серію %d ''The Long Bright Dark''\n",TrueDetevctiv);
+            printf("Дата виходу 12 січня 2014 року");
             break;
         case 2:
-            printf("Ви вибрали серію %d\n", TrueDetevctiv);
-            printf("Дата виходу %d.01.2026", TrueDetevctiv);
+            printf("Ви вибрали серію %d ''Seeing Things''\n", TrueDetevctiv);
+            printf("Дата виходу 19 січня 2014 року");
             break;
         case 3:
-            printf("Ви вибрали серію %d\n", TrueDetevctiv);
-            printf("Дата виходу %d.01.2026", TrueDetevctiv);
+            printf("Ви вибрали серію %d ''The Locked Room''\n", TrueDetevctiv);
+            printf("Дата виходу 26 січня 2014 року");
             break;
         case 4:
-            printf("Ви вибрали серію %d\n", TrueDetevctiv);
-            printf("Дата виходу %d.01.2026", TrueDetevctiv);
+            printf("Ви вибрали серію %d ''Who Goes There''\n", TrueDetevctiv);
+            printf("Дата виходу 9 лютого 2014 року");
             break;
         case 5:
-            printf("Ви вибрали серію %d\n", TrueDetevctiv);
-            printf("Дата виходу %d.01.2026", TrueDetevctiv);
+            printf("Ви вибрали серію %d ''The Secret Fate of All Life''\n", TrueDetevctiv);
+            printf("Дата виходу 16 лютого 2014 року");
             break;
         case 6:
-            printf("Ви вибрали серію %d\n", TrueDetevctiv);
-            printf("Дата виходу %d.01.2026", TrueDetevctiv);
+            printf("Ви вибрали серію %d ''Haunted Houses''\n", TrueDetevctiv);
+            printf("Дата виходу 23 лютого 2014 року");
             break;
         case 7:
-            printf("Ви вибрали серію %d\n", TrueDetevctiv);
-            printf("Дата виходу %d.01.2026", TrueDetevctiv);
+            printf("Ви вибрали серію %d ''After You've Gone''\n", TrueDetevctiv);
+            printf("Дата виходу 2 березня 2014 року");
             break;
         case 8:
-            printf("Ви вибрали серію %d\n", TrueDetevctiv);
-            printf("Дата виходу %d.01.2026", TrueDetevctiv);
-            break;
-        case 9:
-            printf("Ви вибрали серію %d\n", TrueDetevctiv);
-            printf("Дата виходу %d.01.2026", TrueDetevctiv);
-            break;
-        case 10:
-            printf("Ви вибрали серію %d\n", TrueDetevctiv);
-            printf("Дата виходу %d.01.2026", TrueDetevctiv);
-            break;
-        case 11:
-            printf("Ви вибрали серію %d\n", TrueDetevctiv);
-            printf("Дата виходу %d.01.2026", TrueDetevctiv);
-            break;
-        case 12:
-            printf("Ви вибрали серію %d\n", TrueDetevctiv);
-            printf("Дата виходу %d.01.2026", TrueDetevctiv);
+            printf("Ви вибрали серію %d ''Form and Void''\n", TrueDetevctiv);
+            printf("Дата виходу 9 березня 2014 року");
             break;
         default:
             printf("Такої серії немає");
